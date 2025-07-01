496526c37f0a0faab133f4fd6563d6b34696f92a
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-07-01 13:49:17 +0530

    imported Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './component/Navbar/Navbar';
+import Home from './component/Home/Home.js';
 const App = () => {
   return (
     <main className='w-full'>
       <Navbar />
+      <div id='home' className='w-[100%] min-h-[calc(100dvh-70px)]  px-[10%] text-white flex flex-col justify-center items-center max-sm:px-[15px]'>
+        <Home />
+      </div>
     </main>
   )
 }
