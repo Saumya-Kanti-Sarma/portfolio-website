cbef05b89dba17463c41f57568bc20e861144f82
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-07-02 10:09:23 +0530

    removed onclick and added logic

--- src/utils/Btn/Button.tsx
+++ src/utils/Btn/Button.tsx
@@ -1,19 +1,37 @@
 interface btnProps {
-  onclick: () => void;
+
   Text: string,
-  bgColor: string,
-  color: string,
 }
 
-const Button: React.FC<btnProps> = ({ onclick = () => { }, Text = "", bgColor = "#fff", color = "#000" }) => {
+const Button: React.FC<btnProps> = ({ Text = "" }) => {
+  const colorMap: Record<string, { bg: string, font: string }> = { //Record<Keys, Type>
+    TypeScript: { bg: "#3178C6", font: "#000" },
+    JavaScript: { bg: "#FEEC16", font: "#000" },
+    Python: { bg: "#769AB9", font: "#fff" },
+    "Next.js": { bg: "#FFFEFE", font: "#000" },
+    React: { bg: "#51C2E3", font: "#fff" },
+    Redux: { bg: "#1A1B1E", font: "#fff" },
+    Fastify: { bg: "#1A1B1E", font: "#fff" },
+    Flask: { bg: "#1A1B1E", font: "#fff" },
+    Tailwind: { bg: "#43AAB1", font: "#fff" },
+    "Node.js": { bg: "#2F6323", font: "#fff" },
+    NestJS: { bg: "#DF214F", font: "#fff" },
+    Supabase: { bg: "#3ECF8E", font: "#000" },
+    MongoDB: { bg: "#3E9C37", font: "#fff" },
+    Cloudflare: { bg: "#F5AB0D", font: "#000" },
+    Firebase: { bg: "#F5AB0D", font: "#000" },
+    AWS: { bg: "#F69C0B", font: "#000" },
+    Docker: { bg: "#0193D6", font: "#fff" },
+  };
+  const { bg, font } = colorMap[Text] || { bg: "#333", font: "#fff" };
   return (
     <button
       className={`px-3 py-1 rounded-md hover:invert-[0.88] transition-all`}
       style={{
-        background: bgColor,
-        color: color
+        background: bg,
+        color: font
       }}
-      onClick={onclick}
+
     >
       {Text}
     </button>
